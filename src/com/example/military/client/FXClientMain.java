1ee5710776713ced108ecc07585b425225e5cda1
Незговоров Илья <nezgovorov1997@yandex.ru>
2026-03-22 23:17:24 +0300

    test

--- src/com/example/military/client/FXClientMain.java
+++ src/com/example/military/client/FXClientMain.java
@@ -28,7 +28,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FXClientMain extends Application {
-//TEST
+    private Integer test;
     private TableView<MilitaryPerson> table;
     private ObservableList<MilitaryPerson> personData;
     private ServerConnector connector;
